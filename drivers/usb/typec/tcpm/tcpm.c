rockchip-linux/kernel

usb: typec: tcpm: add pending event lock
Quickly plug and unplug the Type-C device with DP function test, there
is a low probability of unplugging the Type-C dongle, DP work cannot
acquire the mutex(port->lock), and it will always be stuck in the
tcpm_unregister_altmodes() function during the rest port process, causing
the Type-C device to be inserted again to fail to be detected or the reboot
system to be stuck. The exception process is as follows:
(1) Thread#1: unplug the Type-C device to trigger cc status changes interrupt, in the
    tcpm_pd_event_handler() function, first acquire the mutex(port->lock),
    then start state_machine work, and finally release the mutex(port->lock);
tcpm_cc_change()
  -->tcpm_pd_event_handler()
    -->mutex_lock(&port->lock)  /* step 1 */
    -->_tcpm_cc_change(port, cc1, cc2) /* step 2 */
      -->tcpm_set_state(port, SRC_UNATTACHED, 0)
        -->kthread_queue_work(port->wq, &port->state_machine) /* step 3 */
    --> mutex_unlock(&port->lock)  /* step 4 */

(2) Thread#2: before the execution of "step 2" is completed, DP work calls
    the callback function registered by the tcpm framework to initiate a VDM
    Message, such as Enter Mode Message, in the tcpm_altmode_enter() acquires
    the mutex(port->lock) fails and goes to sleep;
dp_altmode_work()
  --> typec_altmode_enter(dp->alt)
    --> tcpm_altmode_enter()
      --> mutex_lock(&port->lock) /* dp work enter sleep */

(3) After step 4, tcpm_altmode_enter() did not acquire for the mutex(port->lock),
    but was preempted by the state_machine work to enter the reset port process.
    In the reset port process, call the tcpm_unregister_altmodes() function to
    release altmode and cancel dp work. Because tcpm_altmode_enter() did not
    acquire the mutex(port->lock), causing it to be stuck in cancel dp work.
tcpm_state_machine_work()
  -->mutex_lock(&port->lock)
  -->tcpm_reset_port(port)
    -->tcpm_unregister_altmodes(port)
      ......
      --> dp_altmode_remove()
        

drivers/usb/typec/tcpm/tcpm.c
@@ -346,6 +346,7 @@ struct tcpm_port {
 	unsigned long delay_ms;
 
 	spinlock_t pd_event_lock;
+	struct mutex pd_handler_lock;
 	u32 pd_events;
 
 	struct kthread_work event_work;
@@ -2017,6 +2018,13 @@ static int tcpm_altmode_enter(struct typec_altmode *altmode)
 {
 	struct tcpm_port *port = typec_altmode_get_drvdata(altmode);
 	u32 header;
+	int ret = 0;
+
+	mutex_lock(&port->pd_handler_lock);
+	if (tcpm_port_is_disconnected(port)) {
+		ret = -ENODEV;
+		goto unlock;
+	}
 
 	mutex_lock(&port->lock);
 	header = VDO(altmode->svid, 1, CMD_ENTER_MODE);
@@ -2025,14 +2033,22 @@ static int tcpm_altmode_enter(struct typec_altmode *altmode)
 	tcpm_queue_vdm(port, header, NULL, 0);
 	mod_vdm_delayed_work(port, 0);
 	mutex_unlock(&port->lock);
-
-	return 0;
+unlock:
+	mutex_unlock(&port->pd_handler_lock);
+	return ret;
 }
 
 static int tcpm_altmode_exit(struct typec_altmode *altmode)
 {
 	struct tcpm_port *port = typec_altmode_get_drvdata(altmode);
 	u32 header;
+	int ret = 0;
+
+	mutex_lock(&port->pd_handler_lock);
+	if (tcpm_port_is_disconnected(port)) {
+		ret = -ENODEV;
+		goto unlock;
+	}
 
 	mutex_lock(&port->lock);
 	header = VDO(altmode->svid, 1, CMD_EXIT_MODE);
@@ -2042,20 +2058,31 @@ static int tcpm_altmode_exit(struct typec_altmode *altmode)
 	mod_vdm_delayed_work(port, 0);
 	mutex_unlock(&port->lock);
 
-	return 0;
+unlock:
+	mutex_unlock(&port->pd_handler_lock);
+	return ret;
 }
 
 static int tcpm_altmode_vdm(struct typec_altmode *altmode,
 			    u32 header, const u32 *data, int count)
 {
 	struct tcpm_port *port = typec_altmode_get_drvdata(altmode);
+	int ret = 0;
+
+	mutex_lock(&port->pd_handler_lock);
+	if (tcpm_port_is_disconnected(port)) {
+		ret = -ENODEV;
+		goto unlock;
+	}
 
 	mutex_lock(&port->lock);
 	tcpm_queue_vdm(port, header, data, count - 1);
 	mod_vdm_delayed_work(port, 0);
 	mutex_unlock(&port->lock);
 
-	return 0;
+unlock:
+	mutex_unlock(&port->pd_handler_lock);
+	return ret;
 }
 
 static int tcpm_altmode_notify(struct typec_altmode *altmode,
@@ -4773,6 +4800,7 @@ static void tcpm_pd_event_handler(struct kthread_work *work)
 					      event_work);
 	u32 events;
 
+	mutex_lock(&port->pd_handler_lock);
 	mutex_lock(&port->lock);
 
 	spin_lock(&port->pd_event_lock);
@@ -4826,6 +4854,7 @@ static void tcpm_pd_event_handler(struct kthread_work *work)
 	}
 	spin_unlock(&port->pd_event_lock);
 	mutex_unlock(&port->lock);
+	mutex_unlock(&port->pd_handler_lock);
 }
 
 void tcpm_cc_change(struct tcpm_port *port)
@@ -5883,6 +5912,7 @@ struct tcpm_port *tcpm_register_port(struct device *dev, struct tcpc_dev *tcpc)
 
 	mutex_init(&port->lock);
 	mutex_init(&port->swap_lock);
+	mutex_init(&port->pd_handler_lock);
 
 	port->wq = kthread_create_worker(0, dev_name(dev));
 	if (IS_ERR(port->wq))

Supernote-Ratta/kernel_Nomad_Manta

drivers/usb/typec/tcpm/tcpm.c
@@ -367,6 +367,7 @@ struct tcpm_port {
 	enum tcpm_transmit_status tx_status;
 
 	struct mutex swap_lock;		/* swap command lock */
+	struct mutex pending_event_lock;
 	bool swap_pending;
 	bool non_pd_role_swap;
 	struct completion swap_complete;
@@ -2035,6 +2036,13 @@ static int tcpm_altmode_enter(struct typec_altmode *altmode)
 {
 	struct tcpm_port *port = typec_altmode_get_drvdata(altmode);
 	u32 header;
+	int ret = 0;
+
+	mutex_lock(&port->pending_event_lock);
+	if (tcpm_port_is_disconnected(port)) {
+		ret = -ENODEV;
+		goto unlock;
+	}
 
 	mutex_lock(&port->lock);
 	header = VDO(altmode->svid, 1, CMD_ENTER_MODE);
@@ -2043,14 +2051,22 @@ static int tcpm_altmode_enter(struct typec_altmode *altmode)
 	tcpm_queue_vdm(port, header, NULL, 0);
 	mod_vdm_delayed_work(port, 0);
 	mutex_unlock(&port->lock);
-
-	return 0;
+unlock:
+	mutex_unlock(&port->pending_event_lock);
+	return ret;
 }
 
 static int tcpm_altmode_exit(struct typec_altmode *altmode)
 {
 	struct tcpm_port *port = typec_altmode_get_drvdata(altmode);
 	u32 header;
+	int ret = 0;
+
+	mutex_lock(&port->pending_event_lock);
+	if (tcpm_port_is_disconnected(port)) {
+		ret = -ENODEV;
+		goto unlock;
+	}
 
 	mutex_lock(&port->lock);
 	header = VDO(altmode->svid, 1, CMD_EXIT_MODE);
@@ -2060,20 +2076,31 @@ static int tcpm_altmode_exit(struct typec_altmode *altmode)
 	mod_vdm_delayed_work(port, 0);
 	mutex_unlock(&port->lock);
 
-	return 0;
+unlock:
+	mutex_unlock(&port->pending_event_lock);
+	return ret;
 }
 
 static int tcpm_altmode_vdm(struct typec_altmode *altmode,
 			    u32 header, const u32 *data, int count)
 {
 	struct tcpm_port *port = typec_altmode_get_drvdata(altmode);
+	int ret = 0;
+
+	mutex_lock(&port->pending_event_lock);
+	if (tcpm_port_is_disconnected(port)) {
+		ret = -ENODEV;
+		goto unlock;
+	}
 
 	mutex_lock(&port->lock);
 	tcpm_queue_vdm(port, header, data, count - 1);
 	mod_vdm_delayed_work(port, 0);
 	mutex_unlock(&port->lock);
 
-	return 0;
+unlock:
+	mutex_unlock(&port->pending_event_lock);
+	return ret;
 }
 
 static int tcpm_altmode_notify(struct typec_altmode *altmode,
@@ -4793,6 +4820,7 @@ static void tcpm_pd_event_handler(struct kthread_work *work)
 					      event_work);
 	u32 events;
 
+	mutex_lock(&port->pending_event_lock);
 	mutex_lock(&port->lock);
 
 	spin_lock(&port->pd_event_lock);
@@ -4846,6 +4874,7 @@ static void tcpm_pd_event_handler(struct kthread_work *work)
 	}
 	spin_unlock(&port->pd_event_lock);
 	mutex_unlock(&port->lock);
+	mutex_unlock(&port->pending_event_lock);
 }
 
 void tcpm_cc_change(struct tcpm_port *port)
@@ -5903,6 +5932,7 @@ struct tcpm_port *tcpm_register_port(struct device *dev, struct tcpc_dev *tcpc)
 
 	mutex_init(&port->lock);
 	mutex_init(&port->swap_lock);
+	mutex_init(&port->pending_event_lock);
 
 	port->wq = kthread_create_worker(0, dev_name(dev));
 	if (IS_ERR(port->wq))